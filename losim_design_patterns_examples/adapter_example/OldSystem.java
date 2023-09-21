e4758327e65142813caabea9a5e349a96c6bc498
Sabri Koffler <losim.language@outlook.fr>
2023-09-21 18:34:15 +0200

    Create OldSystem.java

---
+++ losim_design_patterns_examples/adapter_example/OldSystem.java
@@ -0,0 +1,9 @@
+package org.losim.examples.design_patterns;
+
+//public simu_lapo OldSystem
+//Existing class
+public class OldSystem {
+ public void oldMethod() {
+     System.out.println("This is the old method from the OldSystem.");
+ }
+}
