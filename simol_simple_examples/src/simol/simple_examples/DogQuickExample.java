d27361585c7c72c5f2cc6f5fd3609920c24b7248
charleskoffler <losim.language@outlook.fr>
2023-09-30 01:58:49 +0200

    Test signature

--- simol_simple_examples/src/simol/simple_examples/DogQuickExample.java
+++ simol_simple_examples/src/simol/simple_examples/DogQuickExample.java
@@ -1,7 +1,7 @@
 package simol.simple_examples;
 
 /*
- * A first simple example of simol. It's simulate a dog, using a simu_rewo class!
+ * A first simple example of simol. It's simulate a dog, using a simu_rewo class
  */
 // public simu_alfoc DogQuickExample
 public class DogQuickExample {
