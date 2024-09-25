045237078d8eeedacb3242425aa6ff496d7f4cf3
charleskoffler <losim.language@outlook.fr>
2024-09-25 14:02:55 +0200

    Rest of commit.

--- simol_compiler/src/main/java/org/simol/simolframework/java/Simu_agent.java
+++ simol_compiler/src/main/java/org/simol/simolframework/java/Simu_agent.java
@@ -7,9 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
- * Simol Framework
- * 2.3 20240920 New sub-roles: @Human_expert_static, @Expert_component_static, and @Static.
- * @author Charles Koffler
  *
  */
 
